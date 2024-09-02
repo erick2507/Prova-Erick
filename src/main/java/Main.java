1e3471aa7afe4fc38be5a7d791b8f3f88726bd94
erick2507 <169068468+erick2507@users.noreply.github.com>
2024-09-02 12:24:35 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+System.out.println("");
+
+
+    }
+System.out.println(" Seja bem vindo ao nosso Site ");
+System.out.println("" Seu nome" é + sobrenome);
+
+if = Erick dos Santos 
+if = 14
+if = 1,70
+if = 60Kg
+
+
+
+
+
+
+
+System.out.println(" Obrigado por usar nosso site ");
+
+
+  
+sc.close();
